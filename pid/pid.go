3324994d6517ad7d35a83ee11af49ec9020f8d4b
OmarTariq612 <az2339913@gmail.com>
2024-01-01 10:42:33 +0200

    pid

--- pid/pid.go
+++ pid/pid.go
@@ -13,8 +13,6 @@ type PID struct {
 	SampleTime    float64
 	MaxIntegral   float64
 	MaxDerivative float64
-	// Input         float64
-	// Output        float64
 }
 
 func NewPID(kp, ki, kd, setPoint, dt float64) *PID {
@@ -31,27 +29,7 @@ func NewPID(kp, ki, kd, setPoint, dt float64) *PID {
 
 func (pid *PID) Compute(input float64) (output float64) {
 	opErr := pid.SetPoint - input
-
-	// newIntegralValue := pid.Integral + opErr*pid.Dt
-	// switch {
-	// case newIntegralValue > pid.MaxIntegral:
-	// 	pid.Integral = pid.MaxIntegral
-	// case newIntegralValue < -pid.MaxIntegral:
-	// 	pid.Integral = -pid.MaxIntegral
-	// default:
-	// 	pid.Integral = newIntegralValue
-	// }
 	pid.Integral = max(-pid.MaxIntegral, min(pid.MaxIntegral, pid.Integral+opErr*pid.Dt))
-
-	// newDerivativeValue := (opErr - pid.LastOpErr) / pid.Dt
-	// switch {
-	// case newDerivativeValue > pid.MaxDerivative:
-	// 	pid.Derivative = pid.MaxDerivative
-	// case newDerivativeValue < -pid.MaxDerivative:
-	// 	pid.Derivative = -pid.MaxDerivative
-	// default:
-	// 	pid.Derivative = newDerivativeValue
-	// }
 	pid.Derivative = max(-pid.MaxDerivative, min(pid.MaxDerivative, (opErr-pid.LastOpErr)/pid.Dt))
 
 	output = (pid.Kp * opErr) + (pid.Ki * pid.Integral) + (pid.Kd * pid.Derivative)
